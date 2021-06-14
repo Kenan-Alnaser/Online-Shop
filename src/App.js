d2fe84c92d904f19ee23702a527f202379594beb
Kenan Alnaser <kenan-na@live.com>
2021-06-14 15:18:43 +0200

    taking out fontawesome

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import Data from "./data.json";
 import ProductList from "./components/ProductList";
-import { FaBeer } from "react-icons/fa";
-import { WiAlien, WiNightAltThunderstorm } from "react-icons/wi";
+// import { FaBeer } from "react-icons/fa";
+// import { WiAlien, WiNightAltThunderstorm } from "react-icons/wi";
 
 const App = () => {
   const [data, setData] = useState(Data);
@@ -28,10 +28,10 @@ const App = () => {
   return (
     <React.Fragment>
       <h1>Welcome to our online store</h1>
-      <h3>
+      {/* <h3>
         Lets go for a <FaBeer /> with <WiAlien /> when it's
         <WiNightAltThunderstorm />
-      </h3>
+      </h3> */}
       <form onSubmit={handleSubmit}>
         <input type="text" onChange={changeHandle} value={userInput} />
         <input type="submit" value="filter" />
